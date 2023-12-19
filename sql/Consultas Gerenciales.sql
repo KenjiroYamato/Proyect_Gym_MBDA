02ac36b0e487611e2b5df0aa1546000c10fc6f4f
CamargoT <juanpablocamargo91@gmail.com>
2023-12-19 07:53:22 -0500

    [UPDATE] 'SONSULTAS GERENCIALES.SQL' Now  Works

--- sql/Consultas Gerenciales.sql
+++ sql/Consultas Gerenciales.sql
@@ -1,25 +1,31 @@
 --Consultar Total de Beneficios de Todas las Sedes
-CREATE OR REPLACE FUNCTION calcular_beneficios_totales(p_fecha_inicial IN DATE, p_fecha_final IN DATE)
+CREATE OR REPLACE FUNCTION Gerencia.calcular_beneficios_totales(p_fecha_inicial IN DATE, p_fecha_final IN DATE)
     RETURN NUMBER
 IS
-    total_ingresos NUMBER(50);
-    total_egresos NUMBER(50);
-    beneficios_totales NUMBER(50);
+    total_ingresos NUMBER(20);
+    total_egresos_sueldos NUMBER(20);
+    total_egresos_inventario NUMBER(20);
+    beneficios_totales NUMBER(20);
 BEGIN
     -- Calcular ingresos (SUM de VALOR en FACTURAS)
-    SELECT NVL(SUM(VALOR), 0)
+    SELECT SUM(VALOR)
     INTO total_ingresos
     FROM Gerencia.FACTURAS
     WHERE MOMENTOPAGO BETWEEN p_fecha_inicial AND p_fecha_final;
 
-    -- Calcular egresos (SUM de VALOR en FACTURAS con DETALLES que indican egreso)
-    SELECT NVL(SUM(VALOR), 0)
-    INTO total_egresos
-    FROM Gerencia.FACTURAS
-    WHERE MOMENTOPAGO BETWEEN p_fecha_inicial AND p_fecha_final;
+    -- Calcular egresos (SUM de VALOR de EGRESOS)
+    SELECT SUM(SUELDO)
+    INTO total_egresos_sueldos
+    FROM Gerencia.CONTRATOS
+    WHERE FECHAINICIO BETWEEN p_fecha_inicial AND p_fecha_final;
+
+    SELECT SUM(PRECIO)
+    INTO total_egresos_inventario
+    FROM Administracion.INVENTARIO
+    WHERE FECHACOMPRA BETWEEN p_fecha_inicial AND p_fecha_final;
 
     -- Calcular beneficios totales (Ingresos - Egresos)
-    beneficios_totales := total_ingresos - total_egresos;
+    beneficios_totales := total_ingresos - total_egresos_sueldos - total_egresos_inventario;
 
     -- Devolver el resultado
     RETURN beneficios_totales;
@@ -33,22 +39,22 @@ END calcular_beneficios_totales;
 DECLARE
     beneficios NUMBER;
 BEGIN
-    beneficios := calcular_beneficios_totales(TO_DATE('2023-01-01', 'YYYY-MM-DD'), TO_DATE('2023-12-31', 'YYYY-MM-DD'));
+    beneficios := Gerencia.calcular_beneficios_totales(TO_DATE('2020-12-01', 'YYYY-MM-DD'), TO_DATE('2023-12-31', 'YYYY-MM-DD'));
     DBMS_OUTPUT.PUT_LINE('Beneficios totales: ' || beneficios);
 END;
 
 CREATE VIEW VistaMembresiasIngresos AS
 SELECT
-    TO_CHAR(MOMENTOPAGO, 'YYYY-MM') AS MES,
+    TO_CHAR(FECHAINICIO, 'YYYY-MM') AS MES,
     COUNT(MEMBRESIAS.FACTURA) AS NUMERO_DE_MEMBRESIAS,
     SUM(FACTURAS.VALOR) AS INGRESOS_GENERADOS
 FROM
     Gerencia.MEMBRESIAS
 JOIN Gerencia.FACTURAS ON MEMBRESIAS.Factura = FACTURAS.ID
 GROUP BY
-    TO_CHAR(MOMENTOPAGO, 'YYYY-MM')
+    TO_CHAR(FECHAINICIO, 'YYYY-MM')
 ORDER BY
-    TO_CHAR(MOMENTOPAGO, 'YYYY-MM');
+    TO_CHAR(FECHAINICIO, 'YYYY-MM');
 
 --Obtener en Orden Decendente que Tipo de Membresias Son las Mas Compradas
 CREATE VIEW VistaMembresiasMasCompradas AS
